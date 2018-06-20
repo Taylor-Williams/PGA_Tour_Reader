d95a0285a587e13176d28bf020fa98685f38b573
Taylor Williams <taylor@thewilliams.net>
2018-06-20 20:03:38 +0000

    creates tournaments correctly if weekend spans 2 months

--- lib/PGA_Tour_Scraper/PGA_Tour_scraper.rb
+++ lib/PGA_Tour_Scraper/PGA_Tour_scraper.rb
@@ -3,6 +3,8 @@ require 'date'
 require 'open-uri'
 require 'nokogiri'
 
+require_relative "../PGA_tournament.rb"
+
 class PGA_Tour_Scraper
   attr_accessor :path, :year, :tournaments #year is in the form of a string
   @@all = []
@@ -29,13 +31,12 @@ class PGA_Tour_Scraper
       month = date_info.children[1].children.text
       if /\d/ =~ month #when tourny weekend has 2 months
         #creates a month object for both scrapes
-        puts "#{month} #{@year}"
         start_date = Date.parse("#{month} #{@year}")
-        puts start_date
+        end_date = Date.parse("#{date_info.children[3].children.text.strip} #{@year}")
+        PGA_Tournament.new(start_date, end_date)
+      else
+        puts date_info.children[3].children.text.strip
       end
-      # puts date_info.children[3].children.text.strip
     end
   end
 end
-
-PGA_Tour_Scraper.new.scrape_tour_page
--- lib/PGA_tournament.rb
+++ lib/PGA_tournament.rb
@@ -2,10 +2,21 @@ class PGA_Tournament
 
   attr_accessor :start_date, :end_date, :purse, :name, :location, :winner, :cup_points, :url
 
-  def initialize(start_date, end_date, attributes = nil)
+  @@all = []
+
+  def initialize(start_date, end_date, attributes = {})
     @start_date = start_date
     @end_date = end_date
-    attributes.each do {|key, value| self.send("#{key}=", value)}
+    attributes.each {|key, value| self.send("#{key}=", value)}
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
   end
 
 end
