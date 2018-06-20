98d80bef6ed38c6c09ee50e2fb9e387d0e38f77f
Taylor Williams <taylor@thewilliams.net>
2018-06-20 18:35:13 +0000

    starting to turn scraped into into Dates for tournament objects

--- lib/PGA_Tour_Scraper/PGA_Tour_scraper.rb
+++ lib/PGA_Tour_Scraper/PGA_Tour_scraper.rb
@@ -1,3 +1,8 @@
+require 'pry'
+require 'date'
+require 'open-uri'
+require 'nokogiri'
+
 class PGA_Tour_Scraper
   attr_accessor :path, :year, :tournaments #year is in the form of a string
   @@all = []
@@ -5,6 +10,7 @@ class PGA_Tour_Scraper
   def initialize(path = "https://www.pgatour.com/tournaments/schedule.html", year = Time.now.strftime("%Y"))
     @path = path
     @tournaments = []
+    @year = year
     save
     scrape_tour_page
   end
@@ -19,8 +25,16 @@ class PGA_Tour_Scraper
 
   def scrape_tour_page
     page = Nokogiri::HTML(open("#{@path}"))
-    binding.pry
-    puts page.class   # => Nokogiri::HTML::Document
+    page.css('.num-date').each do |date_info|
+      month = date_info.children[1].children.text
+      if /\d/ =~ month #when tourny weekend has 2 months
+        #creates a month object for both scrapes
+        puts "#{month} #{@year}"
+        start_date = Date.parse("#{month} #{@year}")
+        puts start_date
+      end
+      # puts date_info.children[3].children.text.strip
+    end
   end
 end
 
