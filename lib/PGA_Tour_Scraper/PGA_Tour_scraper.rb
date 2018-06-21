ff6b0dac8583a2c127caa636aea8fdff6771db24
Taylor Williams <taylor@thewilliams.net>
2018-06-21 00:35:27 +0000

    working on parsing information

--- lib/PGA_Tour_Scraper/PGA_Tour_scraper.rb
+++ lib/PGA_Tour_Scraper/PGA_Tour_scraper.rb
@@ -29,14 +29,36 @@ class PGA_Tour_Scraper
     page = Nokogiri::HTML(open("#{@path}"))
     page.css('.num-date').each do |date_info|
       month = date_info.children[1].children.text
+      days = date_info.children[3].children.text.strip
       if /\d/ =~ month #when tourny weekend has 2 months
         #creates a month object for both scrapes
         start_date = Date.parse("#{month} #{@year}")
-        end_date = Date.parse("#{date_info.children[3].children.text.strip} #{@year}")
-        PGA_Tournament.new(start_date, end_date)
+        start_date = start_date << 12 if start_date.month.between?(10,12)
+        end_date = Date.parse("#{days} #{@year}")
+        end_date = end_date << 12 if start_date.month.between?(10,12)
+        @tournaments << PGA_Tournament.new(start_date, end_date)
       else
-        puts date_info.children[3].children.text.strip
+        days = days.split(" - ")
+        start_date = Date.parse("#{month} #{days[0]} #{@year}")
+        start_date = start_date << 12 if start_date.month.between?(10,12)
+        end_date = Date.parse("#{month} #{days[1]} #{@year}")
+        end_date = end_date << 12 if start_date.month.between?(10,12)
+        @tournaments << PGA_Tournament.new(start_date, end_date)
       end
+    end
+    page.css(".tournament-text").each do |tournament_info|
+      tournament_info.children[1].children[0].text
+      tournament_info.children[1].attributes["href"].value if tournament_info.children[1].attributes["href"]
+      weird_shit = tournament_info.children[4].text.split(/\s{2,}/)
+      weird_shit.shift
+      puts weird_shit
     end
   end
 end
+
+blah = PGA_Tour_Scraper.new
+blah.scrape_tour_page
+# blah.tournaments.each do |tournament|
+#   puts tournament.start_date
+#   puts tournament.end_date
+# end
